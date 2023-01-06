996cbc16d1f3c1694fca62eb5e25338a981073c2
GuilhermeMAG <g.casagrande@uni9.edu.br>
2023-01-06 18:15:51 -0300

    Add server and public

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(80, () => {
+    console.log(`Servidor rodando! http://localhost:${80}`)
+});
